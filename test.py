512bb724cfbb68f0ce17b9c44d85100f5eb66656
Sasha <cahqba@gmail.com>
2020-04-17 21:57:49 +0300

    test

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-a = int(input())
-b = int(input())
-print(a+b)
+names = ['egor', 'ivan', 'masha']
+names.append('lyuba')
+# counter = 0
+# while counter != len(names):
+#     print(names[counter])
+#     counter += 1
+
+for i in names:
+    print(i)
+
+print('Цикл завершён')
